c6748ea5eda0d28a8f43f7326b9bba528c5c986a
IvanIFChen <ivan.if.chen@gmail.com>
2017-01-13 14:14:05 -0500

    Update userinfo.py

--- userinfo.py
+++ userinfo.py
@@ -1,4 +1,4 @@
 ME = "Rohan Pandit" # Your name, as it appears on Facebook
 API_KEY = "LUtPPWxkJsDzWzKXWz" # Api Key from gender-api.com (optional)
-START_DATE = "1/1/16" # date to start analysis from. Format must be DD/MM/YY
-END_DATE = "9/1/16" # date at which analysis ends. Format must be DD/MM/YY
+START_DATE = "1/1/16" # date to start analysis from. Format must be MM/DD/YY
+END_DATE = "9/1/16" # date at which analysis ends. Format must be MM/DD/YY
